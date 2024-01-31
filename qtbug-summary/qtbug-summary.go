9cd9a89ff240457da1e0964226830bb40cdc5e65
Shawn Rutledge <s@ecloud.org>
2024-01-30 17:53:37 -0700

    act as a line filter, or take multiple bug numbers from command line
    how to write a line filter: https://gobyexample.com/line-filters

--- qtbug-summary/qtbug-summary.go
+++ qtbug-summary/qtbug-summary.go
@@ -11,18 +11,9 @@ import (
 	"strings"
 )
 
-func main() {
-	re := regexp.MustCompile("QTBUG-[0-9]+")
-	bugID := ""
-	if len(os.Args) < 2 {
-		reader := bufio.NewReader(os.Stdin)
-		line, _ := reader.ReadString('\n')
-		matches := re.FindAllString(line, -1)
-		bugID = matches[0]
-	} else {
-		bugID = os.Args[1]
-	}
-	jiraClient, _ := jira.NewClient(nil, "https://bugreports.qt.io/")
+var jiraClient, _ = jira.NewClient(nil, "https://bugreports.qt.io/")
+
+func bugDesc (bugID string) string {
 	issue, _, err := jiraClient.Issue.Get(bugID, nil)
 
 	if (err != nil) {
@@ -31,5 +22,22 @@ func main() {
 
 	priority, _, _ := strings.Cut(issue.Fields.Priority.Name, ":")
 
-	fmt.Printf("%s %s %s: %+v\n", issue.Key, issue.Fields.Status.Name, priority, issue.Fields.Summary)
+	return fmt.Sprintf("%s %s %s: %+v", issue.Key, issue.Fields.Status.Name, priority, issue.Fields.Summary)
+}
+
+func main() {
+	re := regexp.MustCompile("QTBUG-[0-9]+")
+	if len(os.Args) < 2 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			matches := re.FindAllString(scanner.Text(), -1)
+			for _, bugID := range matches {
+				fmt.Printf("%s\n", bugDesc(bugID))
+			}
+		}
+	} else {
+		for _, bugID := range os.Args[1:] {
+			fmt.Printf("%s\n", bugDesc(bugID))
+		}
+	}
 }
